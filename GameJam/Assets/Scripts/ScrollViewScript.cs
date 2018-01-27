29a1b65f86a640fd27c24ae62acb853616528103
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2018-01-27 10:40:27 -0500

    Main menu template done

---
+++ GameJam/Assets/Scripts/ScrollViewScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScrollViewScript : MonoBehaviour {
+
+    [SerializeField]
+    Scrollbar scr;
+    public void ChangeObjectState( bool activeness)
+    {
+        scr.value = 1;
+        gameObject.SetActive(activeness);
+        scr.value = 1;
+    }
+}
