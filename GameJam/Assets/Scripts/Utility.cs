956e3dddfd8460c0458affeabc3e624e09e7b732
Agustin75 <agustinconti@fullsail.edu>
2018-01-28 11:51:54 -0500

    Fixed more bugs, UI and removed unnecessary scenes.

--- GameJam/Assets/Scripts/Utility.cs
+++ GameJam/Assets/Scripts/Utility.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Utility : MonoBehaviour {
-
+public class Utility : MonoBehaviour
+{
 	public enum Emotions
-    {
-		Happiness, Anger, Sadness,Fear
-    }
+	{
+		Happiness, Anger, Sadness, Fear
+	}
 
-    public void LoadScene(string scene)
-    {
-        SceneManager.LoadScene(scene);
-    }
-    public static void LoadSceneA(string scene)
-    {
-        SceneManager.LoadScene(scene);
-    }
+	public void LoadScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
 
-    
+	public static void LoadSceneA(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
 }
