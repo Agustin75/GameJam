822d0cf355964c8b53676898cae26c342aa65c9a
Agustin75 <agustinconti@fullsail.edu>
2018-01-27 22:57:07 -0500

    Audio Errors

---
+++ GameJam/Assets/ButtonHoverSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHoverSound : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string HoverSoundEvent;
+    FMOD.Studio.EventInstance HoverSound;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   public void PlayUISound(string Sound)
+    {
+        HoverSound = FMODUnity.RuntimeManager.CreateInstance(HoverSoundEvent);
+        HoverSound.start();
+    }
+
+    void test()
+    {
+
+    }
+}
