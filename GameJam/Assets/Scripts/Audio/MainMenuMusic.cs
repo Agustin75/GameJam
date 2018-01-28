4a799b3cdf071d1162a4755fcd2637f0ba1e5de7
Brandon Cordell Jones <bcjones1@fullsail.edu>
2018-01-27 22:19:11 -0500

    Adding new sounds

---
+++ GameJam/Assets/Scripts/Audio/MainMenuMusic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string PlayerStateEvent;
+    //public static FMOD.Studio.Bus GetBus(String path);
+    FMOD.Studio.EventInstance playerState;
+    FMOD.Studio.Bus playerBus;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+        playerState = FMODUnity.RuntimeManager.CreateInstance(PlayerStateEvent);
+        playerState.start();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDestroy()
+    {
+        StopAllPlayerEvents();
+
+        //--------------------------------------------------------------------
+        // 6: This shows how to release resources when the unity object is 
+        //    disabled
+        //--------------------------------------------------------------------
+        playerState.release();
+    }
+
+
+    void StopAllPlayerEvents()
+    {
+        playerBus = FMODUnity.RuntimeManager.GetBus("bus:/player");
+        //playerState.stop(STOP);
+    }
+}
+
